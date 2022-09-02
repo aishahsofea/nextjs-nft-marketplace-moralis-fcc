edef0bb5fa01b9ff56503b1e3c313cce1a50e610
Aishah Sofea <aishah.s.hanifa@gmail.com>
2022-09-03 01:10:39 +0800

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+
+import { MoralisProvider } from "react-moralis";
+
+import Header from "../components/Header";
+
+const APP_ID = process.env.NEXT_PUBLIC_APP_ID;
+const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL;
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
+      <Header />
+      <Component {...pageProps} />
+    </MoralisProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
